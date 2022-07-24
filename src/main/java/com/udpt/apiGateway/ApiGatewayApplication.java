b76d41f9a6072e84826127906d2bfba73782dd04
BerNardTran <tbkhanh0908@gmail.com>
2022-07-24 22:58:02 +0700

    Init api gateway

---
+++ src/main/java/com/udpt/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package com.udpt.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/customers/all") // add the api path like the example
+						.uri("http://localhost:8001")) // it's the service
+				.build();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
